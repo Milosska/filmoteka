4ad1299cb172c5a4d862e25fcf89c8d96841e6ca
Olga-Melnyk <3717129@ukr.net>
2023-02-02 17:51:10 +0200

    add fetch

--- src/js/components/fetch.js
+++ src/js/components/fetch.js
@@ -1,1 +1,87 @@
 // <!-- Ольга Мельник та Олексій Воробйов -->
+
+// Запит на список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці
+let page = 1;
+
+async function fetchTrending(page) {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  const TOKEN = '51114562faac57108ae3113fba230ec4';
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}trending/movie/day?api_key=${TOKEN}`
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+// Виклик тимчасовий, щоб можна було подивитися результат
+fetchTrending();
+
+// Запит фільму за ключовим словом на головній сторінці
+async function fetchSearch(value, page) {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  const TOKEN = '51114562faac57108ae3113fba230ec4';
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}search/movie?api_key=${TOKEN}&language=en-US&query=${value}&page=${page}&include_adult=false`
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+// Виклик тимчасовий, щоб можна було подивитися результат
+fetchSearch('fistful');
+
+// Запит повної інформації про кінофільм для сторінки кінофільму
+async function fetchMovie(id) {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  const TOKEN = '51114562faac57108ae3113fba230ec4';
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}movie/${id}?api_key=${TOKEN}&language=en-US`
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+// Виклик тимчасовий, щоб можна було подивитися результат
+fetchMovie(35);
+
+// Запиту повної інформації про можливий трейлер на YouTube
+async function fetchVideos(id) {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  const TOKEN = '51114562faac57108ae3113fba230ec4';
+
+  try {
+    const response = await fetch(
+      `${BASE_URL}movie/${id}/videos?api_key=${TOKEN}`
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+// Виклик тимчасовий, щоб можна було подивитися результат
+fetchVideos(35);
