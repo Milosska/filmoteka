7df9710fb6bf752048a718f7cf607107d03037d5
Liudmyla007 <ldatsenko3@gmail.com>
2023-02-03 00:03:47 +0200

    modal card css and js

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,1 +1,30 @@
 // <!-- Люда Даценко та Тетяна Крамаренко -->
+const test = document.querySelector(".test-modal");
+const myOverlay = document.querySelector(".overlay");
+myOverlay.hidden = true;
+
+const myModal = document.querySelector(".modal_card");
+console.log(myModal);
+
+
+const onBtnClick = () => {
+    myOverlay.hidden = false;
+    myModal.hidden = false;
+    
+    document.addEventListener('keydown', (event) => {
+       if (event.key === 'Escape') {
+         myOverlay.hidden = true;
+         myModal.hidden = true;
+       }
+    });
+
+    
+
+};
+
+test.addEventListener("click", onBtnClick);
+myOverlay.addEventListener('click', () => {
+    myOverlay.hidden = true;
+    myModal.hidden = true;
+ }) 
+    
