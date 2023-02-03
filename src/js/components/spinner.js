6c4a136c58d23504bd5dd55ac21f990a3056d881
Yaroslav12002 <my.mail12002@gmail.com>
2023-02-03 17:15:18 +0200

    card render fix
    hbs for card render fixed
    CSS for card render added
    CSS for spinner added

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -1,1 +1,16 @@
 //<!-- Ярослав Михайлов -->
+const refs = {
+  spinner: document.querySelector('.spinner'),
+};
+
+const spinner = {
+  show() {
+    refs.spinner.classList.remove('is-hidden');
+  },
+
+  hide() {
+    refs.spinner.classList.add('is-hidden');
+  },
+};
+
+export default spinner;
