fb8995ea937fd75b34e180e405c6c6d1bf29997b
Liudmyla007 <ldatsenko3@gmail.com>
2023-02-03 08:27:53 +0200

    modal team css js

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,30 +1,46 @@
 // <!-- Люда Даценко та Тетяна Крамаренко -->
-const test = document.querySelector(".test-modal");
+const btnModalFilm = document.querySelector(".test_modal_film");
+const btnModalTeam = document.querySelector(".test_modal_team");
+const modalTeam = document.querySelector(".modal_team");
 const myOverlay = document.querySelector(".overlay");
+const modalClose = document.querySelector(".modal_close");
+const myModal = document.querySelector(".modal_card");
+
 myOverlay.hidden = true;
 
-const myModal = document.querySelector(".modal_card");
-console.log(myModal);
-
-
-const onBtnClick = () => {
-    myOverlay.hidden = false;
-    myModal.hidden = false;
-    
+// прослуховувач на картку фільма:
+const onCardFilmClick = () => {
+  myOverlay.hidden = false;
+  // при натисканні на "Esc" - модальне вікно закривається:
     document.addEventListener('keydown', (event) => {
        if (event.key === 'Escape') {
-         myOverlay.hidden = true;
-         myModal.hidden = true;
+         myOverlay.hidden = true;      
        }
     });
-
-    
-
 };
 
-test.addEventListener("click", onBtnClick);
+btnModalFilm.addEventListener("click", onCardFilmClick);
+
+// при натисканні на оверлей - модальне вікно закривається:
 myOverlay.addEventListener('click', () => {
-    myOverlay.hidden = true;
-    myModal.hidden = true;
- }) 
+  myOverlay.hidden = true; 
+});
+
+// при натисканні "хрестик" - модальне вікно закривається:
+modalClose.addEventListener('click', () => {
+  myOverlay.hidden = true; 
+});
+
+// прослуховувач на "GoIT Students" в футері:
+const onTeamClick = () => {
+  myOverlay.hidden = false;
+  // при натисканні на "Esc" - модальне вікно закривається:
+    document.addEventListener('keydown', (event) => {
+       if (event.key === 'Escape') {
+         myOverlay.hidden = true;      
+       }
+    });
+};
+
+btnModalTeam.addEventListener("click", onTeamClick);
     
