146c284c80c501ea8b78f5c0acd237b0482ecda9
Yaroslav12002 <my.mail12002@gmail.com>
2023-02-03 09:40:12 +0200

    card render refactoring
    JS function added
    Helpers inputed inside this function

--- src/js/components/card-render.js
+++ src/js/components/card-render.js
@@ -3,22 +3,22 @@
 //Виклик шаблону hbs:
 import cardMarkup from '../../templates/film-card.hbs';
 
-const Handlebars = require('handlebars');
+export default function cardsMarkupCreate(moviesArray) {
+  const Handlebars = require('handlebars');
+  const BASEIMGURL = 'https://image.tmdb.org/t/p/w500';
 
-const BASEIMGURL = 'https://image.tmdb.org/t/p/w500';
+  Handlebars.registerHelper('addBaseUrl', function (imgPath) {
+    return BASEIMGURL + imgPath;
+  });
 
-Handlebars.registerHelper('addBaseUrl', function (imgPath) {
-  return BASEIMGURL + imgPath;
-});
+  Handlebars.registerHelper('takeYearFromDate', function (date) {
+    return date ? date.split('-')[0] : 'year n/a';
+  });
 
-Handlebars.registerHelper('takeYearFromDate', function (date) {
-  return date ? date.split('-')[0] : 'year n/a';
-});
+  Handlebars.registerHelper('getGenres', function (genresArray) {
+    const genres = genresArray.join(',');
+    return genres ? genres : 'Genres n/a';
+  });
 
-Handlebars.registerHelper('getGenres', function (genresArray) {
-  const genres = genresArray.join(',');
-  return genres ? genres : 'Genres n/a';
-});
-
-// console.log(cardMarkup(response.results));
-export { cardMarkup };
+  return cardMarkup(moviesArray);
+}
