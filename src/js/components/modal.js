544662d1c062bd7b2a3e25345d28e9661eb16d60
Liudmyla007 <ldatsenko3@gmail.com>
2023-02-03 15:04:23 +0200

    try to fix js

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,46 +1,81 @@
 // <!-- Люда Даценко та Тетяна Крамаренко -->
 const btnModalFilm = document.querySelector(".test_modal_film");
 const btnModalTeam = document.querySelector(".test_modal_team");
-const modalTeam = document.querySelector(".modal_team");
-const myOverlay = document.querySelector(".overlay");
 const modalClose = document.querySelector(".modal_close");
+const myOverlay = document.querySelector(".overlay");
+const modalTeam = document.querySelector(".modal_team");
 const myModal = document.querySelector(".modal_card");
 
-myOverlay.hidden = true;
+// myOverlay.hidden = true;
 
 // прослуховувач на картку фільма:
-const onCardFilmClick = () => {
-  myOverlay.hidden = false;
-  // при натисканні на "Esc" - модальне вікно закривається:
-    document.addEventListener('keydown', (event) => {
+// const onCardFilmClick = () => {
+//   myOverlay.hidden = false;
+//   modalTeam.hidden = true;
+//   // при натисканні на "Esc" - модальне вікно закривається:
+//     document.addEventListener('keydown', (event) => {
+//        if (event.key === 'Escape') {
+//          myOverlay.hidden = true;      
+//        }
+//     });
+  
+// };
+
+btnModalFilm.addEventListener("click", onModalOpen);
+btnModalTeam.addEventListener("click", onModalOpen);
+
+function onModalOpen(event) {
+    myOverlay.hidden = false;
+  // modalTeam.hidden = true;
+  console.log(event.target);
+   if (event.target === myModal) {
+    myModal.hidden = false;
+
+  } else if (event.target === modalTeam) {
+    modalTeam.hidden = false;
+  }
+  document.addEventListener('keydown', (event) => {
        if (event.key === 'Escape') {
-         myOverlay.hidden = true;      
+         myOverlay.hidden = true;
+        myModal.hidden = true;
+         modalTeam.hidden = true;
+       
        }
-    });
-};
-
-btnModalFilm.addEventListener("click", onCardFilmClick);
-
-// при натисканні на оверлей - модальне вікно закривається:
+  });
 myOverlay.addEventListener('click', () => {
   myOverlay.hidden = true; 
+   myModal.hidden = true;
+        modalTeam.hidden = true;
 });
+  modalClose.addEventListener('click', () => {
+    myOverlay.hidden = true; 
+    myModal.hidden = true;
+        modalTeam.hidden = true;
+});
+
+
+}
+
+
+
+// при натисканні на оверлей - модальне вікно закривається:
+// myOverlay.addEventListener('click', () => {
+//   myOverlay.hidden = true; 
+// });
 
 // при натисканні "хрестик" - модальне вікно закривається:
-modalClose.addEventListener('click', () => {
-  myOverlay.hidden = true; 
-});
+// modalClose.addEventListener('click', () => {
+//   myOverlay.hidden = true; 
+// });
 
 // прослуховувач на "GoIT Students" в футері:
-const onTeamClick = () => {
-  myOverlay.hidden = false;
-  // при натисканні на "Esc" - модальне вікно закривається:
-    document.addEventListener('keydown', (event) => {
-       if (event.key === 'Escape') {
-         myOverlay.hidden = true;      
-       }
-    });
-};
+// const onTeamClick = () => {
+//   myOverlay.hidden = false;
+//   myModal.hidden = true;
 
-btnModalTeam.addEventListener("click", onTeamClick);
+  // при натисканні на "Esc" - модальне вікно закривається:
+
+
+// btnModalTeam.addEventListener("click", onTeamClick);
     
+
