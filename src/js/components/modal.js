486b88ccf50f1c006f82f69ed25adb9a85a21041
Liudmyla007 <ldatsenko3@gmail.com>
2023-02-03 18:44:47 +0200

    add function hideAll()

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,113 +1,45 @@
 // <!-- Люда Даценко та Тетяна Крамаренко -->
-const btnModalFilm = document.querySelector(".test_modal_film");
-const btnModalTeam = document.querySelector(".test_modal_team");
-const modalClose = document.querySelector(".modal_close");
+const btnModalFilm = document.querySelector(".test__modal__film");
+const btnModalTeam = document.querySelector(".test__modal__team");
+const modalClose = document.querySelector(".modal__close");
 const myOverlay = document.querySelector(".overlay");
-const modalTeam = document.querySelector(".modal_team");
-const myModal = document.querySelector(".modal_card");
-
-// myOverlay.hidden = true;
-
-// прослуховувач на картку фільма:
-// const onCardFilmClick = () => {
-//   myOverlay.hidden = false;
-//   modalTeam.hidden = true;
-//   // при натисканні на "Esc" - модальне вікно закривається:
-//     document.addEventListener('keydown', (event) => {
-//        if (event.key === 'Escape') {
-//          myOverlay.hidden = true;      
-//        }
-//     });
-  
-// };
+const modalTeam = document.querySelector(".modal__team");
+const modalCard = document.querySelector(".modal__card");
 
 btnModalFilm.addEventListener("click", onModalOpen);
 btnModalTeam.addEventListener("click", onModalOpen);
 
 function onModalOpen(event) {
-    myOverlay.hidden = false;
+  myOverlay.hidden = false;
    if (event.target === btnModalFilm) {
-     myModal.hidden = false;
+     modalCard.hidden = false;
      modalTeam.hidden = true;
-
-  } else  {
-    modalTeam.hidden = false;
-  }
+    } else  {
+     modalTeam.hidden = false;
+   }
   document.addEventListener('keydown', (event) => {
-       if (event.key === 'Escape') {
-         myOverlay.hidden = true;
-        myModal.hidden = true;
-         modalTeam.hidden = true;
-       
-       }
+     if (event.key === 'Escape') {
+        myOverlay.hidden = true;
+        modalCard.hidden = true;
+        modalTeam.hidden = true;
+      }
   });
-myOverlay.addEventListener('click', () => {
+   myOverlay.addEventListener('click', () => {
+     hideAll();
+  });
+   modalClose.addEventListener('click', () => {
+     hideAll();
+  });
+}
+
+function hideAll(){
   myOverlay.hidden = true; 
-   myModal.hidden = true;
-        modalTeam.hidden = true;
-});
-  modalClose.addEventListener('click', () => {
-    myOverlay.hidden = true; 
-    myModal.hidden = true;
-        modalTeam.hidden = true;
-});
-
-
+  modalCard.hidden = true;
+  modalTeam.hidden = true;
 }
 
 
-// function onModalOpen(event) {
-//     myOverlay.hidden = false;
-//   // modalTeam.hidden = true;
-//   console.log(event.target);
-//    if (event.target === myModal) {
-//     myModal.hidden = false;
-
-//   } else if (event.target === modalTeam) {
-//     modalTeam.hidden = false;
-//   }
-//   document.addEventListener('keydown', (event) => {
-//        if (event.key === 'Escape') {
-//          myOverlay.hidden = true;
-//         myModal.hidden = true;
-//          modalTeam.hidden = true;
-       
-//        }
-//   });
-// myOverlay.addEventListener('click', () => {
-//   myOverlay.hidden = true; 
-//    myModal.hidden = true;
-//         modalTeam.hidden = true;
-// });
-//   modalClose.addEventListener('click', () => {
-//     myOverlay.hidden = true; 
-//     myModal.hidden = true;
-//         modalTeam.hidden = true;
-// });
-
-
-// }
 
 
 
-// при натисканні на оверлей - модальне вікно закривається:
-// myOverlay.addEventListener('click', () => {
-//   myOverlay.hidden = true; 
-// });
-
-// при натисканні "хрестик" - модальне вікно закривається:
-// modalClose.addEventListener('click', () => {
-//   myOverlay.hidden = true; 
-// });
-
-// прослуховувач на "GoIT Students" в футері:
-// const onTeamClick = () => {
-//   myOverlay.hidden = false;
-//   myModal.hidden = true;
-
-  // при натисканні на "Esc" - модальне вікно закривається:
-
-
-// btnModalTeam.addEventListener("click", onTeamClick);
-    
 
