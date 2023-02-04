caa27fa60291eea60f1dcf005a00fa89c5ac4f3d
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-02-04 16:12:54 +0200

    adds
    Подгрузил библиотеку пагинации, подключил стили линком библиотеки,

--- src/js/components/markup-render.js
+++ src/js/components/markup-render.js
@@ -1,4 +1,4 @@
- // 1) Функція, що малює розмітку мейна при переході на сторінку або використанні пагінації - Ірина;
+// 1) Функція, що малює розмітку мейна при переході на сторінку або використанні пагінації - Ірина;
 
 import { fetchInfo } from './fetch.js';
 import cardsMarkupCreate from './card-render';
@@ -10,22 +10,21 @@ formBtn.addEventListener('click', renderPageMarkup);
 window.addEventListener('keydown', onEnterPress);
 // https://api.themoviedb.org/3/trending/movie/day?api_key=51114562faac57108ae3113fba230ec4&page=2
 
-function renderPageMarkup(evt, query, page=1) {
+function renderPageMarkup(evt, query, page = 1) {
   clearMarkup();
-  
-  fetchInfo('keyword', query, page).then(data => {
-              
-    localStorage.setItem('current-films', JSON.stringify(data.results));
 
-    const markup = cardsMarkupCreate(data.results);
+  fetchInfo('keyword', query, page)
+    .then(data => {
+      localStorage.setItem('current-films', JSON.stringify(data.results));
 
-    mainList.insertAdjacentHTML('beforeend', markup);
-       
-    }).catch (error => onFetchError());
+      const markup = cardsMarkupCreate(data.results);
+
+      mainList.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => onFetchError());
 }
 
 function onEnterPress(evt) {
- 
   if (evt.code === 'Enter') {
     renderPageMarkup();
   }
@@ -36,7 +35,7 @@ function clearMarkup() {
 }
 
 function onFetchError() {
-  console.log(error);  
+  console.log(error);
 }
 
 export { renderPageMarkup };
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,1 +1,49 @@
 // <!-- Герман -->
+import Pagination from 'tui-pagination';
+import { fetchInfo } from './components/fetch';
+
+// В ФУНКЦИЮ ПЕРЕДАВАТЬ ОБЬЕКТ(resp), В КОТОРОМ НАХОДИТСЯ КОЛИЧЕСТВО ВСЕХ НАЙДЕННЫХ ЕЛЕМЕНТОВ, ТЕКУЩУЮ СТРАНИЦУ, КОТОРУЮ ОТОБРАЖАТЬ ПРИ ЗАГРУЗКЕ
+function createAPagination(data) {
+  const paganation = new Pagination(
+    document.getElementById('tui-pagination-container'),
+    {
+      page: data.page,
+      totalItems: data.total_results,
+      itemsPerPage: 20,
+      usageStatistics: false,
+      visiblePages: 5,
+      centerAlign: true,
+      template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage:
+          '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+        moveButton:
+          '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</a>',
+        disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</span>',
+        moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+          '<span class="tui-ico-ellip">...</span>' +
+          '</a>',
+      },
+    }
+  );
+  paganation.on('afterMove', ({ page }) => {
+    const currentPage = page;
+    //   ЭТО ВЫЗОВ ФЕТЧА С СТРАНИЦЕЙ, КОТОРУЮ ВЫБИРАЕТ КЛИЕНТ НА ПОЛЗУНКЕ
+    //   НУЖНО ОТРИСОВАТЬ НОВУЮ СТРАНИЦУ ФИЛЬМОВ
+    try {
+      //   fetchInfo('keyword', 'Batman', currentPage).then(resp =>
+      //     console.log(resp)
+      //   );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+export { createAPagination };
