4279adb410baeb42d7ff1e7fc488fb61f11d3686
IvanSosnin1983 <IvanSosnin1983@gmail.com>
2023-02-04 03:51:52 +0200

    add function

--- src/js/user-data.js
+++ src/js/user-data.js
@@ -1,3 +1,35 @@
 //<!-- Іван -->
 // 1) За натисканням на кнопку "Watched" показуються переглянуті фільми користувача
 // 2) За натисканням на кнопку "Queue" показуються фільми додані в чергу користувача
+import cardsMarkupCreate from './components/card-render';
+
+const btn = document.querySelector('.header-library__btn');
+const mainList = document.querySelector('.main__list');
+const WATCHED_KEY = 'watched';
+const QUEUE_KEY = 'queue';
+let currentKey = WATCHED_KEY;
+
+btn.addEventListener('click', onLibraryBtn);
+
+function onLibraryBtn(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  if (evt.target.textContent.toUpperCase() === 'QUEUE') {
+    currentKey = QUEUE_KEY;
+  } else if (evt.target.textContent.toUpperCase() === 'WATCHED') {
+    currentKey = WATCHED_KEY;
+  }
+  const storage = localStorage.getItem(currentKey);
+  if (storage === null) {
+    console.log('Nothing added to the library. Local storage is empty');
+  } else {
+    try {
+      const moviesArray = JSON.parse(storage);
+      const markup = cardsMarkupCreate(moviesArray);
+      mainList.innerHTML = markup;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
