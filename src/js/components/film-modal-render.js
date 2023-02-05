d51012891a1331bb081f1ff588c76d60fb868d88
Oleksandr Husliakov <110162593+AlexGusl97@users.noreply.github.com>
2023-02-05 12:51:33 +0200

    upd
    Поки без JS, бо він щось вимахується

--- src/js/components/film-modal-render.js
+++ src/js/components/film-modal-render.js
@@ -4,3 +4,23 @@
 
 //Виклик шаблону hbs:
 // import filmMarkup from '../../templates/film-modal.hbs';
+// Алекс Гусляков
+// 1) Функція, що рендерить шаблон модалки одного фільму;
+// 2) Після натискання на картку фільму на будь-якій сторінці повинна відкриватися модалка з динамічно підставленою інформацією про фільм
+
+// import filmMarkup from '../../templates/film-modal.hbs';
+// import fetchInfo from './fetch';
+// const body = document.querySelector('body');
+// const modalWindow = document.querySelector('.modal-one-film');
+// const fetchInfo = document.querySelector('.home-container');
+
+function closeFilmModal() {
+  modalWindow.classList.remove('open');
+  wrapper.innerHTML = '';
+  body.classList.remove('content-hidden');
+}
+
+// заборона листати контент за модалкою
+function contentHidden() {
+  body.classList.add('content-hidden');
+}
